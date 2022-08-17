b0104e696f7f5dcbd8dcc72affccc8ed5d1fa2f5
JY <geethabscb@gmail.com>
2022-08-17 23:31:21 +0530

    this is my first code push in git aaaa

--- mongotest.py
+++ mongotest.py
@@ -10,6 +10,21 @@ d = {
     "email":'sudhanshu@ineuron.ai',
     "surname":'kumar'
 }
+d = {
+    "name":'sudhanshu',
+    "email":'sudhanshu@ineuron.ai',
+    "surname":'kumar'
+}
+d = {
+    "name":'sudhanshu',
+    "email":'sudhanshu@ineuron.ai',
+    "surname":'kumar'
+}
+d = {
+    "name":'sudhanshu',
+    "email":'sudhanshu@ineuron.ai',
+    "surname":'kumar'
+}
 db1 = client["mongotest"]
 coll = db1["test"]
 coll.insert_one(d)
